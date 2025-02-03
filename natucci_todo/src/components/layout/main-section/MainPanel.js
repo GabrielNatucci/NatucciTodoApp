0f7cc0fb8b617164495937c11309994279561670
GabrielNatucci <gabriel.natuccirusso@hotmail.com>
2025-02-03 20:01:20 -0300

    adição de uma tabela para as tasks

--- natucci_todo/src/components/layout/main-section/MainPanel.js
+++ natucci_todo/src/components/layout/main-section/MainPanel.js
@@ -3,14 +3,18 @@ import Tasks from '../tasks/Tasks';
 import './mainPanel.css'
 
 const MainPanel = () => {
+
+    const processarInput = () => {
+        console.log("uuuu");
+    }
+
     return (
         <section className="main-section">
             <div className="main-section-quick-add">
                 <input className="main-section-quick-add-input" type="text" placeholder="Nome da sua tarefa"/>
                 <input id = "main-section-quick-add-date" className="main-section-quick-add-input " type="text" placeholder="Ex: 11:00"/>
-                <button className="main-section-quick-add-button">+</button>
+                <button className="main-section-quick-add-button" onClick={processarInput}>+</button>
             </div>
-
             <Tasks/>
         </section>
     )
--- natucci_todo/src/components/layout/tasks/Tasks.js
+++ natucci_todo/src/components/layout/tasks/Tasks.js
@@ -1,10 +1,11 @@
 import { Table } from "react-bootstrap";
 import Task from "./task/Task";
+import './Tasks.css'
 
 const Tasks = () => {
     return (
         <div className="tasks">
-            <Table respnosive striped bordered hover variant="dark">
+            <Table responsive striped bordered hover variant="dark">
                 <thead>
                     <tr>
                         <th>#</th>
@@ -14,12 +15,6 @@ const Tasks = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <Task id="1" content="alimentar o gato" day="02/03/25" time={"19:45:25"} />
-                    <Task id="1" content="estudar react" day="02/03/25" time={"19:45:25"} />
-                    <Task id="1" content="penasr em uma melhor solução para aquele problema do trabalho" day="02/03/25" time={"19:45:25"} />
-                    <Task id="1" content="teste" day="02/03/25" time={"19:45:25"} />
-                    <Task id="1" content="teste" day="02/03/25" time={"19:45:25"} />
-                    <Task id="1" content="teste" day="02/03/25" time={"19:45:25"} />
                 </tbody>
             </Table>
         </div>
